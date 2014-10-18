8a171604865f5e1e307976badd7922dd3b917c1b
S <s>
2014-10-17 22:53:55 -0700

    {"type":"TI", "runs":"3"}

--- BlankClass.java
+++ BlankClass.java
@@ -1,13 +1,6 @@
-/*
- * File: BlankClass.java
- * ---------------------
- * This class is a blank one that you can change at will. Remember, if you change
- * the class name, you'll need to change the filename so that it matches.
- * Then you can extend GraphicsProgram, ConsoleProgram, or DialogProgram as you like.
- */
 
 /*
- * File: Breakout.java
+ * File: BreakoutExtensions.java
  * -------------------
  * Name:
  * Section Leader:
@@ -70,26 +63,49 @@ public class BlankClass extends GraphicsProgram {
 /* Method: run() */
 /** Runs the Breakout program. */
 	public void run() {
-		Canvas C1 = new Canvas();
-		C1.setSize(APPLICATION_WIDTH,APPLICATION_HEIGHT);
-		C1.setBackground(Color.white);
-		makeBall();
-		makePaddle();
+		setUpGame();
 		addMouseListeners();
-		placeBricks();
-		while(ball.getY()<APPLICATION_HEIGHT - PADDLE_Y_OFFSET){
-			ball.move(vx,vy);
-			pause(10);
-			checkForPaddle();
-			checkForSide();
-			checkForBrick();
-		}
+		playTheGame();
 	}	
 
 	/*
-	  * This allows the paddle to move along the x-axis as the mouse moves.(non-Javadoc)
-	  * @see acm.program.Program#mouseMoved(java.awt.event.MouseEvent)
-	  */
+	 * setUpGame allows the BREAKOUT game window to be setup so that
+	 * play can take place.
+	 */
+	
+	private void setUpGame(){
+		makeBall();
+		makePaddle();
+		placeBricks();
+	}
+	
+	/*
+	 * playTheGame is the method that is executed so that the game runs.
+	 */
+	private void playTheGame(){
+		while(ball.getY()<APPLICATION_HEIGHT - PADDLE_Y_OFFSET){
+			ball.move(vx,vy);
+			pause(10);
+			makeChecks();
+		}
+	}
+	
+	/*
+	 * makeChecks allows the ball to consider collisions with bricks,the sides
+	 * of the game window and the paddle.
+	 */
+	private void makeChecks(){
+		checkForPaddle();
+		checkForBrick();
+		checkForSide();
+	}
+	
+	/*
+	 * This method allows the ball to check whether or not it has collided with
+	 * the paddle so that it may ricochet off the paddle, allowing the game to 
+	 * continue.
+	 */
+	
 	private void checkForPaddle(){
 		double xPositionOfBall = ball.getX() + 0.5*BALL_RADIUS;
 		double yPositionOfBall = ball.getY() + BALL_RADIUS;
@@ -102,17 +118,58 @@ public class BlankClass extends GraphicsProgram {
 		}
 	}
 	
+	/*
+	 * This method allows the ball to check whether or not it has collided with
+	 * a brick and then result in the removal of this brick if a collision 
+	 * occurred.		
+	 */
+	
 	private void checkForBrick(){
-		if (getElementAt(ball.getX()+0.5*BALL_RADIUS, ball.getY()) == fifthBricks){
-			remove(fifthBricks);
+		for (int i = 0; i<NBRICK_ROWS ; i++){
+			if (getElementAt(ball.getX()+0.5*BALL_RADIUS, ball.getY()) == arr[i]) {
+				remove(arr[i]);
+				vy = -vy;
+			}else if(getElementAt(ball.getX()+0.5*BALL_RADIUS, ball.getY()) == arr1[i]){
+				remove(arr1[i]);
+				vy = -vy;
+			}else if (getElementAt(ball.getX()+0.5*BALL_RADIUS, ball.getY()) == arrG[i]){
+				remove(arrG[i]);
+				vy = -vy;
+			}else if (getElementAt(ball.getX()+0.5*BALL_RADIUS, ball.getY()) == arrG1[i]){
+				remove(arrG1[i]);
+				vy = -vy;
+			}else if (getElementAt(ball.getX()+0.5*BALL_RADIUS, ball.getY()) == arrY[i]){
+				remove(arrY[i]);
+				vy = -vy;
+			}else if(getElementAt(ball.getX()+0.5*BALL_RADIUS, ball.getY()) == arrY1[i]){
+				remove(arrY1[i]);
+				vy = -vy;
+			}else if (getElementAt(ball.getX()+0.5*BALL_RADIUS, ball.getY()) == arrO[i]){
+				remove(arrO[i]);
+				vy = -vy;
+			}else if (getElementAt(ball.getX()+0.5*BALL_RADIUS, ball.getY()) == arrO1[i]){
+				remove(arrO1[i]);
+				vy = -vy;
+			}else if (getElementAt(ball.getX()+0.5*BALL_RADIUS, ball.getY()) == arrR[i]){
+				remove(arrR[i]);
+				vy = -vy;
+			} else if (getElementAt(ball.getX()+0.5*BALL_RADIUS, ball.getY()) == arrR1[i]){
+				remove(arrR1[i]);
+				vy = -vy;
+			}
+			
+			
 		}
 	}
+	
 	
 	/*
 	 * This is needed to keep the ball within the dimensions of the screen.
 	 * More specifically, it allows the ball to ricochet off the walls of the
 	 * application.
 	 */
+	
+	
 	private void checkForSide(){
 		if (ball.getX()+ 2*BALL_RADIUS > APPLICATION_WIDTH ){
 			vx = -vx;
@@ -124,6 +181,7 @@ public class BlankClass extends GraphicsProgram {
 			vy = -vy;
 		}
 	}
+	
 	
 	public void mouseMoved(MouseEvent e) {
 		 
@@ -154,7 +212,12 @@ public class BlankClass extends GraphicsProgram {
 	
 	private RandomGenerator rgen = RandomGenerator.getInstance();
 	
-	private GRect fifthBricks;
+	
+	private GRect [] arr, arr1;
+	private GRect [] arrG, arrG1;
+	private GRect [] arrY, arrY1;
+	private GRect [] arrO, arrO1;
+	private GRect [] arrR, arrR1;
 	
 	/*
 	 * The paddle used to hit the ball towards the bricks is created here.
@@ -165,46 +228,117 @@ public class BlankClass extends GraphicsProgram {
 		paddle.setFillColor(Color.BLACK);
 		add(paddle);
 	}
+	
 	/*
 	 * The bricks placed at the top of the program are created here.
 	 */
-	private void placeBricks(){
-		for (int r = 0; r<2;r++){
-			for (int i = 0;i<NBRICK_ROWS;i++){
-				GRect bricks = new GRect (i*(BRICK_SEP+BRICK_WIDTH),r*(BRICK_HEIGHT+BRICK_SEP)+BRICK_Y_OFFSET,BRICK_WIDTH,BRICK_HEIGHT);
-		//		boolean filled = true;
-				bricks.setColor (Color.RED);
-				bricks.setFilled(true);
-				bricks.setFillColor(Color.RED);
-				add(bricks);
-				
-				GRect secondBricks = new GRect (i*(BRICK_SEP+BRICK_WIDTH),r*((BRICK_HEIGHT+BRICK_SEP))+2*(BRICK_HEIGHT+BRICK_SEP)+BRICK_Y_OFFSET,BRICK_WIDTH,BRICK_HEIGHT);
-				secondBricks.setColor (Color.ORANGE);
-				secondBricks.setFilled(true);
-				secondBricks.setFillColor(Color.ORANGE);
-				add(secondBricks);
-				
-				GRect thirdBricks = new GRect (i*(BRICK_SEP+BRICK_WIDTH),r*((BRICK_HEIGHT+BRICK_SEP))+4*(BRICK_HEIGHT+BRICK_SEP)+BRICK_Y_OFFSET,BRICK_WIDTH,BRICK_HEIGHT);
-				thirdBricks.setColor (Color.YELLOW);
-				thirdBricks.setFilled(true);
-				thirdBricks.setFillColor(Color.YELLOW);
-				add(thirdBricks);
-				
-				GRect fourthBricks = new GRect (i*(BRICK_SEP+BRICK_WIDTH),r*((BRICK_HEIGHT+BRICK_SEP))+6*(BRICK_HEIGHT+BRICK_SEP)+BRICK_Y_OFFSET,BRICK_WIDTH,BRICK_HEIGHT);
-				fourthBricks.setColor (Color.GREEN);
-				fourthBricks.setFilled(true);
-				fourthBricks.setFillColor(Color.GREEN);
-				add(fourthBricks);
-				
-				GRect fifthBricks = new GRect (i*(BRICK_SEP+BRICK_WIDTH),r*((BRICK_HEIGHT+BRICK_SEP))+8*(BRICK_HEIGHT+BRICK_SEP)+BRICK_Y_OFFSET,BRICK_WIDTH,BRICK_HEIGHT);
-				fifthBricks.setColor(Color.CYAN);
-				fifthBricks.setFilled(true);
-				fifthBricks.setFillColor(Color.CYAN);
-				add(fifthBricks);
-			}	
-		}	
+	
+	private void placeRedBricks(){
+		arrR = new GRect[NBRICK_ROWS];
+		for (int k =0; k<NBRICK_ROWS ; k++){
+			arrR[k] = new GRect(k*(BRICK_SEP+BRICK_WIDTH),-1*((BRICK_HEIGHT+BRICK_SEP))+(BRICK_HEIGHT+BRICK_SEP)+BRICK_Y_OFFSET,BRICK_WIDTH,BRICK_HEIGHT);
+			arrR[k].setColor(Color.RED);
+			arrR[k].setFilled(true);
+			arrR[k].setFillColor(Color.RED);
+			add(arrR[k]);
+		}
+		arrR1 = new GRect[NBRICK_ROWS];
+		for (int k =0; k<NBRICK_ROWS ; k++){
+			arrR1[k] = new GRect(k*(BRICK_SEP+BRICK_WIDTH),0*((BRICK_HEIGHT+BRICK_SEP))+(BRICK_HEIGHT+BRICK_SEP)+BRICK_Y_OFFSET,BRICK_WIDTH,BRICK_HEIGHT);
+			arrR1[k].setColor(Color.RED);
+			arrR1[k].setFilled(true);
+			arrR1[k].setFillColor(Color.RED);
+			add(arrR1[k]);
+		}
+	}
+	
+	private void placeOrangeBricks(){
+		arrO = new GRect[NBRICK_ROWS];
+		for (int k =0; k<NBRICK_ROWS ; k++){
+			arrO[k] = new GRect(k*(BRICK_SEP+BRICK_WIDTH),((BRICK_HEIGHT+BRICK_SEP))+2*(BRICK_HEIGHT+BRICK_SEP)+BRICK_Y_OFFSET,BRICK_WIDTH,BRICK_HEIGHT);
+			arrO[k].setColor(Color.ORANGE);
+			arrO[k].setFilled(true);
+			arrO[k].setFillColor(Color.ORANGE);
+			add(arrO[k]);
+		}
+		arrO1 = new GRect[NBRICK_ROWS];
+		for (int k =0; k<NBRICK_ROWS ; k++){
+			arrO1[k] = new GRect(k*(BRICK_SEP+BRICK_WIDTH),0*((BRICK_HEIGHT+BRICK_SEP))+2*(BRICK_HEIGHT+BRICK_SEP)+BRICK_Y_OFFSET,BRICK_WIDTH,BRICK_HEIGHT);
+			arrO1[k].setColor(Color.ORANGE);
+			arrO1[k].setFilled(true);
+			arrO1[k].setFillColor(Color.ORANGE);
+			add(arrO1[k]);
+		}
+	}
+	
+	private void placeYellowBricks(){
+		arrY = new GRect[NBRICK_ROWS];
+		for (int k =0; k<NBRICK_ROWS ; k++){
+			arrY[k] = new GRect(k*(BRICK_SEP+BRICK_WIDTH),((BRICK_HEIGHT+BRICK_SEP))+4*(BRICK_HEIGHT+BRICK_SEP)+BRICK_Y_OFFSET,BRICK_WIDTH,BRICK_HEIGHT);
+			arrY[k].setColor(Color.YELLOW);
+			arrY[k].setFilled(true);
+			arrY[k].setFillColor(Color.YELLOW);
+			add(arrY[k]);
+		}
+		arrY1 = new GRect[NBRICK_ROWS];
+		for (int k =0; k<NBRICK_ROWS ; k++){
+			arrY1[k] = new GRect(k*(BRICK_SEP+BRICK_WIDTH),0*((BRICK_HEIGHT+BRICK_SEP))+4*(BRICK_HEIGHT+BRICK_SEP)+BRICK_Y_OFFSET,BRICK_WIDTH,BRICK_HEIGHT);
+			arrY1[k].setColor(Color.YELLOW);
+			arrY1[k].setFilled(true);
+			arrY1[k].setFillColor(Color.YELLOW);
+			add(arrY1[k]);
+		}
 		
 	}
 	
-}
+	
+	private void placeGreenBricks(){
+		arrG = new GRect[NBRICK_ROWS];
+		for (int k =0; k<NBRICK_ROWS ; k++){
+			arrG[k] = new GRect(k*(BRICK_SEP+BRICK_WIDTH),((BRICK_HEIGHT+BRICK_SEP))+6*(BRICK_HEIGHT+BRICK_SEP)+BRICK_Y_OFFSET,BRICK_WIDTH,BRICK_HEIGHT);
+			arrG[k].setColor(Color.GREEN);
+			arrG[k].setFilled(true);
+			arrG[k].setFillColor(Color.GREEN);
+			add(arrG[k]);
+		}
+		arrG1 = new GRect[NBRICK_ROWS];
+		for (int k =0; k<NBRICK_ROWS ; k++){
+			arrG1[k] = new GRect(k*(BRICK_SEP+BRICK_WIDTH),0*((BRICK_HEIGHT+BRICK_SEP))+6*(BRICK_HEIGHT+BRICK_SEP)+BRICK_Y_OFFSET,BRICK_WIDTH,BRICK_HEIGHT);
+			arrG1[k].setColor(Color.GREEN);
+			arrG1[k].setFilled(true);
+			arrG1[k].setFillColor(Color.GREEN);
+			add(arrG1[k]);
+		}
+		
+		
+	}
 
+	
+	private void placeCyanBricks(){
+		arr = new GRect[NBRICK_ROWS];
+		arr1 = new GRect[NBRICK_ROWS];
+			for (int k =0; k<NBRICK_ROWS ; k++){
+				arr[k] = new GRect(k*(BRICK_SEP+BRICK_WIDTH),((BRICK_HEIGHT+BRICK_SEP))+8*(BRICK_HEIGHT+BRICK_SEP)+BRICK_Y_OFFSET,BRICK_WIDTH,BRICK_HEIGHT);
+				arr[k].setColor(Color.CYAN);
+				arr[k].setFilled(true);
+				arr[k].setFillColor(Color.CYAN);
+				add(arr[k]);
+			}
+			for (int k =0; k<NBRICK_ROWS ; k++){
+				arr1[k] = new GRect(k*(BRICK_SEP+BRICK_WIDTH),0*((BRICK_HEIGHT+BRICK_SEP))+8*(BRICK_HEIGHT+BRICK_SEP)+BRICK_Y_OFFSET,BRICK_WIDTH,BRICK_HEIGHT);
+				arr1[k].setColor(Color.CYAN);
+				arr1[k].setFilled(true);
+				arr1[k].setFillColor(Color.CYAN);
+				add(arr1[k]);
+			}	
+	}
+	
+	
+	private void placeBricks(){
+		placeRedBricks();
+		placeOrangeBricks();
+		placeYellowBricks();
+		placeGreenBricks();
+		placeCyanBricks();
+	}
+}
